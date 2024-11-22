e3d94ee9cbbe40d8569136c57829c936fde8c9ce
Abdallah Askar <lalaaskar678@gmail.com>
2024-11-22 13:20:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// navbar resposive
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+
+function focus() {
+    let a = document.getElementById("demo").style.borderRadius= "20px"; 
+
+    }
+
+
+
+
+
+    
+
